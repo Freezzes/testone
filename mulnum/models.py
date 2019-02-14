1b5ec69957c065fdafb7e003bac6874319b072b9
wanvipa <wanvipa1906@gmail.com>
2019-02-14 15:26:44 +0700

    has textbox without result

--- mulnum/models.py
+++ mulnum/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class number(models.Model):
+    numtext = models.CharField(max_length=200)
+    count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.numtext
--- mulnum/views.py
+++ mulnum/views.py
@@ -6,6 +6,7 @@ from django.template import loader
 from django.views import generic
 from django.urls import reverse
 from django.utils import timezone
+from .models import number
 
 # Create your views here.
 
@@ -14,31 +15,45 @@ def inputnum(request):
     return render(request,'mulnum/index.html')
 
 #number from url
-def show(request,id):
-    number = []
-    for i in range(1,13):
-        ans = id * i
-        number.append(ans)
-    context = {
-        'id': id,
-        'num': number
-    }
-    return render(request, 'mulnum/showmul.html', context)
+# def show(request,id):
+#     number = []
+#     for i in range(1,13):
+#         ans = id * i
+#         number.append(ans)
+#     context = {
+#         'id': id,
+#         'num': number
+#     }
+#     return render(request, 'mulnum/showmul.html', context)
 
 #number from textbox
+# def mul(request):
+#     number = []
+#     for i in range(1,13):
+#         ans = int(request.POST['inte']) * i
+#         number.append(ans)
+#     context = {
+#         'id': request.POST['inte'],
+#         'num': number
+#     }
+#     return render(request, 'mulnum/showmul.html', context)
+
 def mul(request):
-    number = []
-    for i in range(1,13):
-        ans = int(request.POST['inte']) * i
-        number.append(ans)
+    ans = number.objects.get(request.POST['number'])
+    numbers=[]
+    for i in range(1,13): 
+        numbers.append(int(ans) * i)
+
     context = {
-        'id': request.POST['inte'],
-        'num': number
+        'id': request.POST['number'],
+        'num': numbers
     }
     return render(request, 'mulnum/showmul.html', context)
 
+
+
 def calstat(request):
-    k = request.POST['inte'] 
+    k = request.POST['number'] 
     d={}
     all_k = []
     all_k.append(k)
@@ -54,3 +69,20 @@ def calstat(request):
     return render(request, 'mulnum/statistic.html', context)
     #return HttpResponseRedirect(reverse('m:often'))
 
+# def vote(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     try:
+#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+#     except (KeyError, Choice.DoesNotExist):
+#         # Redisplay the question voting form.
+#         return render(request, 'polls/detail.html', {
+#             'question': question,
+#             'error_message': "You didn't select a choice.",
+#         })
+#     else:
+#         selected_choice.votes += 1
+#         selected_choice.save()
+#         # Always return an HttpResponseRedirect after successfully dealing
+#         # with POST data. This prevents data from being posted twice if a
+#         # user hits the Back button.
+#         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
